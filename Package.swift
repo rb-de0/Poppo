558ed8edebd4e6a386636383859b0af8c32b82d4
rb_de0 <rebirth.de0@gmail.com>
2017-07-01 17:33:04 +0900

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Poppo",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/BlueCryptor", majorVersion: 0, minor: 7)
+        .Package(url: "https://github.com/IBM-Swift/BlueCryptor", majorVersion: 0, minor: 8)
     ]
 )
